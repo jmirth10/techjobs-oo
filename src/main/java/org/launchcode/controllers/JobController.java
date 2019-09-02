c88ca0d2f50f898894a71df6d8d0374f40a3979c
Joseph Mirth <mirthlacrosse@gmail.com>
2019-09-02 11:15:42 -0500

    Fixed typo in newjob

--- src/main/java/org/launchcode/controllers/JobController.java
+++ src/main/java/org/launchcode/controllers/JobController.java
@@ -20,7 +20,7 @@ public class JobController {
     private JobData jobData = JobData.getInstance();
 
     // The detail display for a given Job at URLs like /job?id=17
-    @RequestMapping(value = "", method = RequestMethod.GET)
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index(Model model, int id) {
 
         // TODO #1 - get the Job with the given ID and pass it into the view
